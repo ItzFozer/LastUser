7f24c31cc8123d9bd5292852ef963d4865b4179c
ItzFozer <50673642+ItzFozer@users.noreply.github.com>
2019-06-09 03:27:06 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ client.on('message', message => {
             var Canvas = module.require('canvas');
             var jimp = module.require('jimp');
    
-     const w = ['./ID1.png.PNG','./ID2.png.PNG','./ID3.png.PNG','./ID4.png.PNG','./ID5.png.PNG'];
+     const w = ['./ID1.png','./ID2.png','./ID3.png','./ID4.png','./ID5.png'];
    
              let Image = Canvas.Image,
                  canvas = new Canvas(802, 404),
